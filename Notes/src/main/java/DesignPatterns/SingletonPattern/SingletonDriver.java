e59ceb69603c5cf64e3bd09b8755cfeea6484ea7
David Amateau <Damateeau@gmail.com>
2021-03-21 18:43:08 -0400

    adding singleton pattern

---
+++ Notes/src/main/java/DesignPatterns/SingletonPattern/SingletonDriver.java
@@ -0,0 +1,8 @@
+package DesignPatterns.SingletonPattern;
+
+public class SingletonDriver {
+
+    public static void main(String[] args) {
+        SingletonEmergencyMessage.getInstance().emergencyMessge();
+    }
+}
---
+++ Notes/src/main/java/DesignPatterns/SingletonPattern/SingletonEmergencyMessage.java
@@ -0,0 +1,27 @@
+package DesignPatterns.SingletonPattern;
+
+/**
+ * The Singleton is a design pattern that allows you to have one single instance of an object.
+ * It's constructor is set to private so no other class can instantiate a new instance.
+ * The unique instance is accessed through a get method. You can use 'Lazy Construction' on the
+ * Singleton if memory space is a concern that will only create the single instance of the
+ * Singleton when and if it is called.
+ */
+
+public class SingletonEmergencyMessage {
+
+    private static SingletonEmergencyMessage uniqueInstance;
+
+    private SingletonEmergencyMessage(){}
+
+    public static synchronized SingletonEmergencyMessage getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new SingletonEmergencyMessage();
+        }
+        return uniqueInstance;
+    }
+
+    public void emergencyMessge() {
+        System.out.println("This is a unique emergency message. Cats have taken over the world.");
+    }
+}
