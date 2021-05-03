a1e50eb429088bd7a53b58f70c94d61529c563e3
David Amateau <damateau@gmail.com>
2021-05-02 22:49:45 -0400

    adding hackerrank problem

---
+++ Notes/src/main/java/HackerRank/ReverseList/ReverseAList.java
@@ -0,0 +1,67 @@
+package HackerRank.ReverseList;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> arr) {
+        List<Integer> reverse = new ArrayList<>();
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            reverse.add(arr.get(i));
+        }
+        return reverse;
+    }
+
+}
+
+public class ReverseAList {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.reverseArray(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
