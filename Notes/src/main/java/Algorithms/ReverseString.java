fe623d01b8b14376657204a7f3d88d5bf583c23e
David Amateau <damateau@gmail.com>
2021-07-21 17:30:54 -0400

    simple reverse char array

---
+++ Notes/src/main/java/Algorithms/ReverseString.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright David Amateau 2021.
+ */
+
+package Algorithms;
+
+public class ReverseString {
+
+    public void reverseString(char[] charArray) {
+        int aPointer = 0;
+        int bPointer = charArray.length - 1;
+
+        while (aPointer <= bPointer) {
+            char temp = charArray[aPointer];
+            charArray[aPointer] = charArray[bPointer];
+            charArray[bPointer] = temp;
+
+            aPointer++;
+            bPointer--;
+        }
+
+    }
+}
