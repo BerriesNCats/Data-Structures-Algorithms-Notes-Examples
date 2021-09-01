73a9c73ef92d482a8be41963cecf2f94525cbe04
David Amateau <damateau@gmail.com>
2021-09-01 14:44:15 -0400

    creating a class to store useful regular expressions for reference

--- Notes/src/main/java/RegularExpressions/MyRegExLibrary.java
+++ Notes/src/main/java/RegularExpressions/MyRegExLibrary.java
@@ -5,18 +5,14 @@ import java.util.regex.Pattern;
 
 public class MyRegExLibrary {
 
-
+    final static Pattern singleSentencePattern = Pattern.compile("[A-Z][^\\.?!]+[\\.?!]");
 
     public static void main(String[] args) {
-
-        String singleSentence = "I walked the cat to the moon today!";
-        System.out.println(isSingleSentence(singleSentence));
+        System.out.println(isSingleSentence("I walked the cat to the moon Today!"));
 
     }
 
     private static boolean isSingleSentence(String singleSentence) {
-        Pattern singleSentencePattern = Pattern.compile("[A-Z][^\\.?!]+[\\.?!]");
-        Matcher singleSentenceMatcher = singleSentencePattern.matcher(singleSentence);
-        return singleSentenceMatcher.matches();
+        return singleSentencePattern.matcher(singleSentence).matches();
     }
 }
