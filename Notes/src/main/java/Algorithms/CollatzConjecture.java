c3bfec15de7ff724a4b7a4257c839799becceaed
David Amateau <damateau@gmail.com>
2021-09-02 22:54:27 -0400

    adding collatz conjecture

---
+++ Notes/src/main/java/Algorithms/CollatzConjecture.java
@@ -0,0 +1,27 @@
+package Algorithms;
+
+public class CollatzConjecture {
+
+  public static void main(String[] args) {
+    collatzConjecture(458);
+  }
+
+  public static void collatzConjecture(int number) {
+    int numberOfSteps = 0;
+
+    while (number != 1) {
+      numberOfSteps++;
+      if (isEven(number)) {
+        number = number / 2;
+      } else {
+        number = number * 3 + 1;
+      }
+      System.out.println(number);
+    }
+    System.out.println("Number of Steps to termination: " + numberOfSteps);
+  }
+
+  private static boolean isEven(int number) {
+    return number % 2 == 0;
+  }
+}
